3892feb9e2643c4f15d44169fdd532c260c2ab71
mike <savvateevmisha@mail.ru>
2023-01-26 22:42:03 +0300

    Update Fenwick

--- struct/Fenwick.cpp
+++ struct/Fenwick.cpp
@@ -8,18 +8,21 @@ struct Fen {
     }
 
     void plus(int q, int x) {
-        q++;
-        for (; q < n; q += (q & (-q))) {
+        for (++q; q < n; q += (q & -q)) {
             fen[q] += x;
         }
     }
 
     int sum(int q) {
-        int ans = 0;
-        for (; q > 0; q -= (q & (-q))) {
-            ans += fen[q];
+        int res = 0;
+        for (; q > 0; q -= (q & -q)) {
+            res += fen[q];
         }
-        return ans;
+        return res;
+    }
+
+    int sum(int l, int r) {
+        return sum(r)-sum(l);
     }
 };
 
