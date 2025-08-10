f64b527be2bb5ecc17ed7fe0c2721c49fd85a011
me <savvateev.ma@phystech.edu>
2025-08-10 06:33:31 +0300

    Rewrite geometry.

---
+++ math/geometry/basic.cpp
@@ -0,0 +1,154 @@
+const ld E = 1e-8;
+
+template <typename T>
+struct NumT {
+    T val;
+
+    NumT(T val): val(val) {}
+
+    operator T() {
+        return val;
+    }
+
+    bool operator==(const NumT& other) const {
+        return abs(val-other.val) < E;
+    }
+};
+
+using Num = NumT<ld>;
+
+auto operator<=>(const Num& x, const Num& y) {
+    if (x == y) {
+        return x.val <=> x.val;
+    }
+    return x.val <=> y.val;
+}
+
+istream& operator>>(istream& in, Num& x) {
+    int val;
+    cin >> val;
+    x.val = val;
+    return in;
+}
+
+int sign(Num x) {
+    return (x > Num(0))-(x < Num(0));
+}
+
+struct Pt {
+    Num x = 0, y = 0;
+
+    bool operator==(const Pt& other) const {
+        return x == other.x && y == other.y;
+    }
+
+    Pt operator+(Pt other) {
+        return {x+other.x, y+other.y};
+    }
+
+    Pt operator-(Pt other) {
+        return {x-other.x, y-other.y};
+    }
+
+    Pt operator*(Num t) {
+        return {x*t, y*t};
+    }
+};
+
+Num dot(Pt x, Pt y) {
+    return x.x*y.x+x.y*y.y;
+}
+
+Num cross(Pt x, Pt y) {
+    return x.x*y.y-x.y*y.x;
+}
+
+Num dist_2(Pt x, Pt y) {
+    return dot(y-x, y-x);
+}
+
+ld angle(Pt x) {
+    ld alpha = atan2l(x.y, x.x);
+    ld pi = numbers::pi_v<ld>;
+    return alpha+2*pi*(alpha < -E);
+}
+
+struct Line {
+    Pt a, dir;
+
+    int side(Pt x) {
+        return sign(cross(dir, x-a));
+    }
+
+    Pt proj(Pt x) {
+        ld t = dot(x-a, dir)/dot(dir, dir);
+        return a+dir*t;
+    }
+};
+
+Line make_line(Pt x, Pt y) {
+//    ld t = 1/sqrtl(dist_2(x, y));
+    int t = 1;
+    return {x, (y-x)*t};
+}
+
+Pt inter(Line line1, Line line2) {
+    Num det = cross(line1.dir, line2.dir);
+    if (det == Num(0)) {
+        return {NAN, NAN};
+    }
+    Pt r = line2.a-line1.a;
+    Num det_x = cross(r, line2.dir);
+    ld t = det_x/det;
+    return line1.a+line1.dir*t;
+}
+
+bool on_seg(Pt a, Pt b, Pt x) {
+    return cross(b-a, x-a) == Num(0) && dot(a-x, b-x) <= Num(0);
+}
+
+struct Cir {
+    Pt c;
+    Num r = 0;
+
+    int in(Pt x) {
+        return sign(r*r-dist_2(c, x));
+    }
+};
+
+vector<Pt> inter(Cir cir, Line line) {
+    Pt proj = line.proj(cir.c);
+    Num d_2 = dist_2(proj, cir.c);
+    Num r_2 = cir.r*cir.r;
+    if (d_2 > r_2) {
+        return {};
+    }
+    if (d_2 == r_2) {
+        return {proj};
+    }
+    Pt dir = line.dir;
+    ld t = sqrtl((r_2-d_2)/dot(dir, dir));
+    return {proj-dir*t, proj+dir*t};
+}
+
+Line rad_axis(Cir cir1, Cir cir2) {
+    Num d_2 = dist_2(cir1.c, cir2.c);
+    Num r_2 = cir1.r*cir1.r-cir2.r*cir2.r;
+    ld t = (d_2+r_2)/(2*d_2);
+    Pt dir = cir2.c-cir1.c;
+    return {cir1.c+dir*t, {-dir.y, dir.x}};
+}
+
+vector<Pt> inter(Cir cir1, Cir cir2) {
+    if (cir1.c == cir2.c) {
+        return {};
+    }
+    return inter(cir1, rad_axis(cir1, cir2));
+}
+
+vector<Pt> tangents(Cir cir, Pt x) {
+    Num r_2 = dist_2(cir.c, x)-cir.r*cir.r;
+    ld r = sqrtl(max<ld>(0, r_2));
+    return inter({x, r}, cir);
+}
+
--- math/geometry/polygons.cpp
+++ math/geometry/polygons.cpp
@@ -1,198 +1,132 @@
-#define ld long double
-
-struct Pt {
-    int x, y;
-
-    Pt(): x(0), y(0) {}
-    Pt(int x, int y): x(x), y(y) {}
-
-    auto operator<=>(const Pt& other) const = default;
-
-    Pt operator+(const Pt& point) const {
-        return {x+point.x, y+point.y};
-    }
-
-    Pt operator-(const Pt& point) const {
-        return {x-point.x, y-point.y};
-    }
-};
-
-istream& operator>>(istream& in, Pt& point) {
-    in >> point.x >> point.y;
-    return in;
-}
-
-ostream& operator<<(ostream& out, const Pt& point) {
-    out << point.x << ' ' << point.y;
-    return out;
-}
-
-int dot(Pt x, Pt y) {
-    return x.x*y.x+x.y*y.y;
-}
-
-int cross(Pt x, Pt y) {
-    return x.x*y.y-x.y*y.x;
-}
-
-ld abs(Pt x) {
-    return hypot(x.x, x.y);
-}
-
-int abs_2(Pt x) {
-    return x.x*x.x+x.y*x.y;
-}
-
-ld dist(Pt x, Pt y) {
-    return abs(x-y);
-}
-
-int dist_2(Pt x, Pt y) {
-    return abs_2(x-y);
-}
-
-ld angle(Pt x, Pt y) {
-    // angle between (1, 0) и (sin, cos)
-    return atan2(cross(x, y), dot(x, y));
-}
-
-bool on_line(Pt x, Pt y, Pt z) {
-    return cross(y-x, z-x) == 0;
-}
-
-struct Seg {
-    Pt x, y;
-
-    Seg(Pt x, Pt y): x(x), y(y) {}
-
-    auto operator<=>(const Seg& other) const = default;
-
-    explicit operator bool() const {
-        return x != y;
-    }
-};
-
-bool on_seg(Seg q, Pt point) {
-    return on_line(q.x, q.y, point) && dot(point-q.x, point-q.y) <= 0;
-}
-
-int S_tr_2(Pt x, Pt y, Pt z) {
-    return cross(y-x, z-x);
-}
-
 struct Polygon {
     vector<Pt> a;
+    int n;
 
-    explicit Polygon(vector<Pt> a_ = {}): a(std::move(a_)) {
-        if (a.empty()) {
+    Polygon(const vector<Pt>& a_): a(a_) {
+        auto w = std::unique(a.begin(), a.end());
+        n = w-a.begin();
+        if (n == 0) {
             return;
         }
-        normalize();
-        if (a.size() > 1) {
-            a.push_back(a[0]);
-        }
-    }
-
-    void counterclockwise() {
-        if (S_2() < 0) {
-            reverse(a.begin(), a.end());
-        }
-    }
-
-    void normalize() {
-        int n = (int)a.size();
-        int ind = 1;
-        while (ind < n-1 && on_line(a[0], a[ind], a[ind+1])) {
-            ind++;
-        }
-        if (ind >= n-1) {
-            Pt min1 = *min_element(a.begin(), a.end());
-            Pt max1 = *max_element(a.begin(), a.end());
-            a = (min1 == max1 ? vector{min1} : vector{min1, max1});
-            return;
-        }
-        rotate(a.begin(), a.begin()+ind, a.end());
+        n -= (n > 1 && a[0] == a[n-1]);
+        a.resize(n);
         a.push_back(a[0]);
-        vector<Pt> will_a = {a[0]};
-        for (int q = 1; q < n; q++) {
-            if (!on_line(will_a.back(), a[q], a[q+1])) {
-                will_a.push_back(a[q]);
-            }
-        }
-        a = will_a;
-    }
-
-    vector<Seg> get_edges() const {
-        int n = (int)a.size();
-        vector<Seg> edges;
-        for (int q = 1; q < n; q++) {
-            edges.emplace_back(a[q-1], a[q]);
-        }
-        return edges;
-    }
-
-    bool on_border(Pt point) const {
-        return ranges::any_of(get_edges(), [point](Seg q) {return on_seg(q, point);});
-    }
-
-    int belonging(Pt point) const { // 0 -> out, 1 -> in, 2 -> border
-        if (on_border(point)) {
-            return 2;
-        }
-        ld ang = 0;
-        for (Seg q : get_edges()) {
-            ang += angle(q.x-point, q.y-point);
-        }
-        // will be 0 or 2*pi
-        return abs(ang) > numbers::pi_v<ld>;
-    }
-
-    int S_2() const {
-        int ans = 0;
-        for (Seg q : get_edges()) {
-            ans += S_tr_2(a[0], q.x, q.y);
-        }
-        return ans;
     }
 };
 
-bool need_pop_back(Pt x, vector<Pt>& ans, bool up) {
-    int m = (int)ans.size(), sign = 2*up-1;
-    return m >= 2 && cross(ans[m-1]-ans[m-2], x-ans[m-2])*sign >= 0;
-}
-
-void make_envelope(vector<Pt>& a, bool up) {
-    int n = (int)a.size();
-    vector<Pt> ans = {a[0]};
-    for (int q = 1; q < n; q++) {
-        while (need_pop_back(a[q], ans, up)) {
-            ans.pop_back();
-        }
-        ans.push_back(a[q]);
+int belong_convex(Polygon& a, Pt x) {
+    if (a.n <= 1) {
+        return (a.n == 1 && a.a[0] == x)-1;
     }
-    a = ans;
-}
-
-Polygon convex_hull(vector<Pt> a) {
-    int n = (int)a.size();
-    sort(a.begin(), a.end());
-    if (n == 0 || a[0] == a.back()) {
-        return Polygon(n == 0 ? vector<Pt>{} : vector{a[0]});
+    Pt pt = a.a[0];
+    if (cross(a.a[1]-pt, x-pt) <= Num(0)) {
+        return on_seg(a.a[0], a.a[1], x)-1;
     }
-    vector<Pt> up = {a[0]}, down = {a[0]};
-    for (int q = 1; q < n-1; q++) {
-        int value = cross(a.back()-a[0], a[q]-a[0]);
-        if (value > 0) {
-            up.push_back(a[q]);
-        } else if (value < 0) {
-            down.push_back(a[q]);
+    if (cross(a.a[a.n-1]-pt, x-pt) >= Num(0)) {
+        return on_seg(a.a[0], a.a[a.n-1], x)-1;
+    }
+    int l = 0, r = a.n;
+    while (r-l > 1) {
+        int m = (l+r)/2;
+        if (cross(a.a[m]-pt, x-pt) > Num(0)) {
+            l = m;
+        } else {
+            r = m;
         }
     }
-    up.push_back(a.back());
-    down.push_back(a.back());
-    make_envelope(up, true);
-    make_envelope(down, false);
-    up.insert(up.end(), down.rbegin()+1, down.rend()-1);
-    return Polygon(up);
+    return make_line(a.a[l], a.a[r]).side(x);
+}
+
+Polygon convex_hull(vector<Pt>& A) {
+    if (A.empty()) {
+        return {{}};
+    }
+    Pt pt = ranges::min(A, {}, [](Pt x) {return pair{x.x.val, x.y.val};});
+    auto aa = views::filter(A, [&pt](Pt x) {return x != pt;});
+    vector<Pt> a(aa.begin(), aa.end());
+    ranges::sort(a, [&pt](Pt x, Pt y) {
+        Num cr = cross(x-pt, y-pt);
+        return cr > Num(0) || cr == Num(0) && dot(pt-x, y-x) < Num(0);
+    });
+    vector<Pt> hull = {pt};
+    for (Pt q : views::join(vector{a, {pt}})) {
+        while (hull.size() > 1) {
+            Pt prev = hull[(int)hull.size()-2];
+            if (cross(hull.back()-prev, q-prev) > Num(0)) {
+                break;
+            }
+            hull.pop_back();
+        }
+        hull.push_back(q);
+    }
+    if (hull.size() == 2 && !a.empty()) {
+        return {{pt, a.back()}};
+    }
+    return {{hull.begin(), hull.end()-1}};
+}
+
+bool add_line(deque<Line>& hull, Line line) {
+    int len = (int)hull.size();
+    while (len > 1 && line.side(inter(hull[len-1], hull[len-2])) == -1) {
+        hull.pop_back();
+        len--;
+    }
+    while (len > 1 && line.side(inter(hull[0], hull[1])) == -1) {
+        hull.pop_front();
+        len--;
+    }
+    if (len > 0 && cross(line.dir, hull.back().dir) == Num(0)) {
+        if (dot(line.dir, hull.back().dir) < Num(0)) {
+            return false;
+        }
+        if (cross(hull.back().dir, line.a-hull.back().a) > Num(0)) {
+            hull.pop_back();
+        } else {
+            return true;
+        }
+    }
+    if (len == 1 && cross(hull[0].dir, line.dir) < Num(0)) {
+        return false;
+    }
+    hull.push_back(line);
+    return true;
+}
+
+vector<Line> inter_lines(vector<Line>& a) {
+    deque<Line> hull;
+    for (Line line : a) {
+        if (!add_line(hull, line)) {
+            return {};
+        }
+    }
+    if (hull.empty() || !add_line(hull, hull[0])) {
+        return {};
+    }
+    return {hull.begin(), hull.end()-1};
+}
+
+Polygon half_planes_inter(vector<Line> A) {
+    A.emplace_back(Pt(-INF, -INF), Pt(1, 0));
+    A.emplace_back(Pt(INF, -INF), Pt(0, 1));
+    A.emplace_back(Pt(INF, INF), Pt(-1, 0));
+    A.emplace_back(Pt(-INF, INF), Pt(0, -1));
+
+    vector<pair<Line, ld>> aa;
+    for (Line line : A) {
+        aa.emplace_back(line, angle(line.dir));
+    }
+    ranges::sort(aa, {}, [](auto x) {return x.second;});
+    vector<Line> a;
+    for (auto q : aa) {
+        a.push_back(q.first);
+    }
+    vector<Line> hull = inter_lines(a);
+
+    int n = (int)hull.size();
+    vector<Pt> ans;
+    for (int q = 0; q < n; q++) {
+        ans.push_back(inter(hull[q], hull[(q+1) % n]));
+    }
+    return {ans};
 }
 
