7022c8451331b0dbb1e472ba40d7427d5536ce34
me <savvateev.ma@phystech.edu>
2025-12-16 18:52:12 +0300

    Simplify Implicit Treap.

--- struct/Treap/Implicit_DD.cpp
+++ struct/Treap/Implicit_DD.cpp
@@ -1,131 +1,98 @@
-mt19937 randint(179);
+mt19937 randint(17957179);
 
 struct Node {
-    int x, y;
-    Node *l, *r, *parent;
-    int size, sum;
+    static Node* null;
 
-    Node(int x1): x(x1), y(randint()), l(nullptr), r(nullptr), parent(nullptr), size(1), sum(x) {}
+    int x, y;
+    Node *l, *r;
+    Node* parent;
+    int size;
+
+    Node(int x): x(x), y(randint()), size(1) {
+        if (null == nullptr) {
+            null = this, size = 0;
+        }
+        l = r = parent = null;
+    }
 };
 
+Node* Node::null = nullptr;
+Node* null = new Node(0);
+
 void update(Node* tree) {
-    if (tree == nullptr) {
-        return;
-    }
-    tree->size = 1, tree->sum = tree->x;
-    if (tree->l != nullptr) {
-        tree->size += tree->l->size, tree->sum += tree->l->sum;
-    }
-    if (tree->r != nullptr) {
-        tree->size += tree->r->size, tree->sum += tree->r->sum;
-    }
-}
-
-void change_parent(Node* tree, Node* parent) {
-    if (tree == nullptr) {
-        return;
-    }
-    tree->parent = parent;
-}
-
-void change_left(Node* tree, Node* left) {
-    if (tree == nullptr) {
-        return;
-    }
-    tree->l = left;
-    change_parent(left, tree);
-    update(tree);
-}
-
-void change_right(Node* tree, Node* right) {
-    if (tree == nullptr) {
-        return;
-    }
-    tree->r = right;
-    change_parent(right, tree);
-    update(tree);
+    tree->l->parent = tree->r->parent = tree;
+    tree->size = 1;
+    tree->size += tree->l->size+tree->r->size;
 }
 
 Node* merge(Node* tree1, Node* tree2) {
-    if (tree1 == nullptr) {
-        return tree2;
-    }
-    if (tree2 == nullptr) {
-        return tree1;
+    tree1->parent = tree2->parent = null;
+    if (tree1 == null || tree2 == null) {
+        return tree1 == null ? tree2 : tree1;
     }
     if (tree1->y < tree2->y) {
-        change_parent(tree1->r, nullptr);
-        change_right(tree1, merge(tree1->r, tree2));
+        tree1->r = merge(tree1->r, tree2);
+        update(tree1);
         return tree1;
     }
-    change_parent(tree2->l, nullptr);
-    change_left(tree2, merge(tree1, tree2->l));
+    tree2->l = merge(tree1, tree2->l);
+    update(tree2);
     return tree2;
 }
 
 pair<Node*, Node*> split(Node* tree, int k) {
-    if (tree == nullptr) {
-        return {nullptr, nullptr};
+    tree->parent = null;
+    if (tree == null) {
+        return {null, null};
     }
-    int t = (tree->l != nullptr ? tree->l->size : 0);
-    if (k <= t) {
-        change_parent(tree->l, nullptr);
-        pair<Node*, Node*> trees = split(tree->l, k);
-        change_left(tree, trees.second);
-        return {trees.first, tree};
+    if (tree->l->size < k) {
+        auto [left, right] = split(tree->r, k-tree->l->size-1);
+        tree->r = left;
+        update(tree);
+        return {tree, right};
     }
-    change_parent(tree->r, nullptr);
-    pair<Node*, Node*> trees = split(tree->r, k-t-1);
-    change_right(tree, trees.first);
-    return {tree, trees.second};
+    auto [left, right] = split(tree->l, k);
+    tree->l = right;
+    update(tree);
+    return {left, tree};
 }
 
-Node* add(Node* tree, int k, Node* vertex) {
-    pair<Node*, Node*> trees = split(tree, k);
-    return merge(merge(trees.first, vertex), trees.second);
+Node* add(Node* tree, int k, int x) {
+    Node* node = new Node(x);
+    auto [left, right] = split(tree, k);
+    return merge(merge(left, node), right);
 }
 
 Node* del(Node* tree, int k) {
-    pair<Node*, Node*> trees1 = split(tree, k);
-    pair<Node*, Node*> trees2 = split(trees1.second, 1);
-    return merge(trees1.first, trees2.second);
+    auto [left, mid] = split(tree, k);
+    auto [node, right] = split(mid, 1);
+    return merge(left, right);
 }
 
-Node* root(Node* tree) {
-    if (tree == nullptr) {
-        return nullptr;
-    }
-    while (tree->parent != nullptr) {
-        tree = tree->parent;
+Node* find_elem(Node* tree, int ind) {
+    while (tree != null && tree->l->size != ind) {
+        if (tree->l->size < ind) {
+            ind -= tree->l->size+1;
+            tree = tree->r;
+        } else {
+            tree = tree->l;
+        }
     }
     return tree;
 }
 
-int find_pos(Node* tree) {
-    if (tree == nullptr) {
-        return 0;
+int find_pos(Node* node) {
+    if (node == null) {
+        return -1;
     }
-    int ans = (tree->l != nullptr ? tree->l->size : 0);
-    while (tree->parent != nullptr) {
-        if (tree->parent->l != tree) {
-            ans += (tree->parent->l != nullptr ? tree->parent->l->size : 0)+1;
+    int pos = node->l->size;
+    while (node->parent != null) {
+        Node* parent = node->parent;
+        if (parent->r == node) {
+            pos += parent->l->size+1;
         }
-        tree = tree->parent;
+        node = parent;
     }
-    return ans;
-}
-
-Node* find_element(Node* tree, int k) {
-    if (tree == nullptr) {
-        return nullptr;
-    }
-    int t = (tree->l != nullptr ? tree->l->size : 0);
-    if (k == t) {
-        return tree;
-    }
-    if (k < t) {
-        return find_element(tree->l, k);
-    }
-    return find_element(tree->r, k-t-1);
+    return pos;
 }
 
